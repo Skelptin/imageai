d7f84bda92b3407d1894b44efeebc54b50294216
Skelptin <mpainkra7@gmail.com>
2024-12-09 17:30:35 -0800

    port changed

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,6 @@ app.get('/', (req, res) => {
     res.send("API Working")
 })
 
-app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT)
-})
-
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
